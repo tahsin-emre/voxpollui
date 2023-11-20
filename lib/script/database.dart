fdd98aa2a52a30a925211d3dd412e5f3e9f096fa
Mehmet Fidan <mehmetfidan889@gmail.com>
2023-11-20 19:01:27 +0300

    Update database.dart

--- lib/script/database.dart
+++ lib/script/database.dart
@@ -3,35 +3,29 @@ import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
 class Database {
   Future<List<Map<String, dynamic>>> fetchPolls() async {
   final queryForPoll = QueryBuilder(ParseObject('Poll'));
-  final queryForPollOption = QueryBuilder(ParseObject('PollOption'));
   final responseForPoll = await queryForPoll.query();
-  final responseForPollOption = await queryForPollOption.query();
 
   if (responseForPoll.success && responseForPoll.results != null) {
-    if (responseForPollOption.success && responseForPollOption.result != null) {
-      List<Map<String, dynamic>> combinedResults = [];
-      for (var poll in responseForPoll.results as List<ParseObject>) {
-        var createdBy = poll.get<String>('createdBy');
-        var creatorData = await fetchCreater(createdBy);
-        combinedResults.add({
-          'poll': poll,
-          'creator': creatorData,
-        });
+    List<Map<String, dynamic>> combinedResults = [];
+    for (var poll in responseForPoll.results as List<ParseObject>) {
+      var createdBy = poll.get<String>('createdBy');
+      ParseObject? creatorData;
+      if (createdBy != null && createdBy.isNotEmpty) {
+        creatorData = await fetchCreater(createdBy);
       }
-      for (var pollOption in responseForPollOption.results as List<ParseObject>) {
-        combinedResults.add({
-          'pollOption': pollOption,
-        });
-      }
-      return combinedResults;
-    } else {
-      return [];
+      combinedResults.add({
+        'poll': poll,
+        'creator': creatorData,
+      });
     }
+    return combinedResults;
   } else {
     // Hata durumunda işlem
     return [];
   }
 }
+
+
 
 Future<ParseObject?> fetchCreater(String? createrId) async {
   if (createrId == null) return null;
