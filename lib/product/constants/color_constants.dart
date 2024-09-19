d65c6075184bc77f2653a5a9fd5e7cf05d1f62dc
Tahsin Emre <telli@tahsinemre.com>
2024-09-19 13:32:10 +0300

    Refactor color and font constants

--- lib/product/constants/color_constants.dart
+++ lib/product/constants/color_constants.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 final class AppColor {
   const AppColor._();
-  static Color NATIONAL_COLOR = const Color.fromARGB(255, 35, 86, 255);
-  static Color INCE_NATIONAL_COLOR = const Color.fromARGB(255, 10, 145, 255);
+  static Color primary = const Color.fromARGB(255, 35, 86, 255);
+  static Color secondary = const Color.fromARGB(255, 10, 145, 255);
 }
--- lib/product/constants/font_constants.dart
+++ lib/product/constants/font_constants.dart
@@ -1,19 +1,10 @@
 final class FontConstants {
   const FontConstants._();
-  static String GILROY_BLACK = "Gilroy";
-  static String GILROY_LIGHT = "Gilroy-light";
-  static String GILROY_MEDIUM = "Gilroy-medium";
-  static String GILROY_BOLD = "Gilroy-bold";
-  static String GILROY_HEAVY = "Gilroy-heavhy";
-  static String GILROY_REGULAR = "Gilroy-regular";
-  static String GILROY_SEMIBOLD = "Gilroy-semibold";
+  static String gilroyBlack = 'Gilroy';
+  static String gilroyLight = 'Gilroy-light';
+  static String gilroyMedium = 'Gilroy-medium';
+  static String gilroyBold = 'Gilroy-bold';
+  static String gilroyHeavy = 'Gilroy-heavhy';
+  static String gilroyRegular = 'Gilroy-regular';
+  static String gilroySemibold = 'Gilroy-semibold';
 }
-
-
-/*
-extension FontConstantsExt on IconData {
-  Icon get toIcon {
-    return Icon(this);
-  }
-}
-*/
