528366aa5dfc74acb40942db0baf67c88894bf0d
Tahsin Emre <telli@tahsinemre.com>
2024-09-19 13:30:29 +0300

    Refactor route and view names in single_routes.dart and route_tree.dart

--- lib/app.dart
+++ lib/app.dart
@@ -2,7 +2,6 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:voxpollui/features/authentication/cubit/auth_cubit.dart';
-import 'package:voxpollui/features/home/view/mehmet/deneme.dart';
 import 'package:voxpollui/features/poll/cubit/poll_cubit.dart';
 import 'package:voxpollui/product/router/app_router.dart';
 
@@ -16,14 +15,13 @@ final class MainApp extends StatelessWidget {
         BlocProvider(create: (_) => AuthCubit()),
         BlocProvider(create: (_) => PollCubit()),
       ],
-      child: MaterialApp(
+      child: MaterialApp.router(
         title: 'Vox Poll',
         localizationsDelegates: context.localizationDelegates,
         supportedLocales: context.supportedLocales,
         locale: context.locale,
         debugShowCheckedModeBanner: false,
-        home: const Page0(),
-        //routerConfig: AppRouter.config,
+        routerConfig: AppRouter.config,
       ),
     );
   }
