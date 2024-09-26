3925b33f56bde6fa8c3da63c7c7792b69c341602
Tahsin Emre <telli@tahsinemre.com>
2024-09-26 14:20:04 +0300

    Refactor community routes and fix typo in community search

--- lib/features/community/view/community_list_view.dart
+++ lib/features/community/view/community_list_view.dart
@@ -5,6 +5,10 @@ import 'package:voxpollui/features/community/cubit/community_cubit.dart';
 import 'package:voxpollui/features/community/cubit/community_state.dart';
 import 'package:voxpollui/features/community/mixin/community_list_mixin.dart';
 import 'package:voxpollui/features/community/widget/community_card.dart';
+import 'package:voxpollui/features/sub_features/app_search/view/app_search_bar.dart';
+import 'package:voxpollui/features/sub_features/common_widgets/extended_elevated_button.dart';
+import 'package:voxpollui/product/initialize/localization/locale_keys.g.dart';
+import 'package:voxpollui/product/utils/constants/page_paddings.dart';
 
 class CommunityListView extends StatefulWidget {
   const CommunityListView({super.key});
@@ -17,16 +21,28 @@ class _CommunityListViewState extends State<CommunityListView>
     with CommunityListMixin {
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<CommunityCubit, CommunityState>(
-      builder: (_, state) {
-        final list = state.communityList ?? [];
-        return Skeletonizer(
-          enabled: state.isLoading,
-          child: ListView(
-            children: [...list.map(CommunityCard.new)],
-          ),
-        );
-      },
+    return SafeArea(
+      child: BlocBuilder<CommunityCubit, CommunityState>(
+        builder: (_, state) {
+          final list = state.communityList ?? [];
+          return Skeletonizer(
+            enabled: state.isLoading,
+            child: ListView(
+              padding: PagePaddings.allL,
+              children: [
+                AppSearchBar(
+                  onChanged: (value) {},
+                  label: LocaleKeys.community_communitySearch,
+                ),
+                ExtendedElevatedButton(
+                    onPressed: () {},
+                    text: LocaleKeys.community_createCommunity),
+                ...list.map(CommunityCard.new),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
---
+++ lib/features/sub_features/common_widgets/extended_elevated_button.dart
@@ -0,0 +1,24 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:voxpollui/product/utils/constants/color_constants.dart';
+import 'package:voxpollui/product/utils/constants/page_paddings.dart';
+
+class ExtendedElevatedButton extends Container {
+  ExtendedElevatedButton({
+    required VoidCallback onPressed,
+    required String text,
+    super.key,
+  }) : super(
+          margin: PagePaddings.allS,
+          child: ElevatedButton(
+              onPressed: onPressed,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: AppColor.primary,
+                foregroundColor: AppColor.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5), // Border radius
+                ),
+              ),
+              child: Text(text.tr())),
+        );
+}
