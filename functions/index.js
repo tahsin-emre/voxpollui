53e97c9bdfd147313bc118e42db8800eeb387371
Tahsin Emre <telli@tahsinemre.com>
2024-10-15 03:20:47 +0300

    Refactor vote and follow functions

--- functions/index.js
+++ functions/index.js
@@ -1,107 +1,74 @@
+/* eslint-disable max-len */
 const {initializeApp} = require("firebase-admin/app");
 const {getFirestore} = require("firebase-admin/firestore");
-const {onRequest} = require("firebase-functions/v2/https");
+const {onDocumentCreated, onDocumentDeleted} = require("firebase-functions/v2/firestore");
 initializeApp();
 const db = getFirestore();
 
-exports.votePoll = onRequest(
-    async (req, res) => {
-      // RequestBody {'userId':string, 'pollId':string, 'optionId':string}
-      const pollId = req.body.pollId;
-      const userId = req.body.userId;
-      const optionId = req.body.optionId;
-      const pollRef = db.collection("polls").doc(pollId);
-      const voteSnap = await pollRef.collection("votes").doc(userId).get();
-      if (voteSnap.exists) {
-        res.status(400).send("User already voted");
-        return;
-      }
-      const pollSnap = await pollRef.get();
-      const pollData = pollSnap.data();
-      const options = pollData.options;
-      const updatedOptions = options.map((option) => {
-        if (option.id === optionId) {
-          return {
-            ...option,
-            voteCount: option.voteCount + 1,
-          };
-        }
-        return option;
-      });
-      await pollRef.collection("votes").doc(userId)
-          .set(
-              {"optionId": optionId,
-                "createdAt": new Date(),
-                "userId": userId},
-          );
-      await pollRef.update({options: updatedOptions});
-      res.status(200).send(true);
-    },
-);
 
-exports.followUser = onRequest(
-    async (req, res) => {
-      // RequestBody {'userId':string, 'followerId':string}
-      const followerId = req.body.followerId;
-      const userId = req.body.userId;
-      const userRef = db.collection("users").doc(userId);
-      const followerRef = db.collection("users").doc(followerId);
-      const followerCollection = userRef.collection("followers");
-      const followingCollection = followerRef.collection("following");
-      await followerCollection.doc(followerId).set({"createdAt": new Date()});
-      await followingCollection.doc(userId).set({"createdAt": new Date()});
-      const userSnap = await userRef.get();
-      const userData = userSnap.data();
-      const followerSnap = await followerRef.get();
-      const followerData = followerSnap.data();
-      await userRef
-          .update({followersCount: userData.followersCount + 1});
-      await followerRef
-          .update({followingCount: followerData.followingCount + 1});
-      res.status(200).send(true);
-    },
-);
+exports.vote = onDocumentCreated("polls/{pollId}/votes/{userId}", async (event) => {
+  const pollId = event.params.pollId;
+  const optionId = event.data.optionId;
+  const pollRef = db.collection("users").doc(pollId);
+  const pollSnap = await pollRef.get();
+  const pollData = pollSnap.data();
+  const options = pollData.options;
+  const updatedOptions = options.map((option) => {
+    if (option.id === optionId) {
+      return {
+        ...option,
+        voteCount: option.voteCount + 1,
+      };
+    }
+    return option;
+  });
+  await pollRef.update({"options": updatedOptions});
+});
 
-exports.unfollowUser = onRequest(
-    async (req, res) => {
-      // RequestBody {'userId':string, 'followerId':string}
-      const followerId = req.body.followerId;
-      const userId = req.body.userId;
-      const userRef = db.collection("users").doc(userId);
-      const followerRef = db.collection("users").doc(followerId);
-      const followerCollection = userRef.collection("followers");
-      const followingCollection = followerRef.collection("following");
-      await followerCollection.doc(followerId).delete();
-      await followingCollection.doc(userId).delete();
-      const userSnap = await userRef.get();
-      const userData = userSnap.data();
-      const followerSnap = await followerRef.get();
-      const followerData = followerSnap.data();
-      await userRef
-          .update({followersCount: userData.followersCount - 1});
-      await followerRef
-          .update({followingCount: followerData.followingCount - 1});
-      res.status(200).send(true);
-    },
-);
+exports.following = onDocumentCreated("users/{userId}/following/{followingId}", async (event) => {
+  const userId = event.params.userId;
+  const userRef = db.collection("users").doc(userId);
+  const userSnap = await userRef.get();
+  const userData = userSnap.data();
+  await userRef.update({followingCount: userData.followingCount + 1});
+});
 
-exports.joinCommunity = onRequest(
-    async (req, res) => {
-    // RequestBody {'userId':string, 'communityId':string}
-      const communityId = req.body.communityId;
-      const userId = req.body.userId;
-      const communityRef = db.collection("communities").doc(communityId);
-      const membersRef = communityRef.collection("members");
-      const communitySnap = await communityRef.get();
-      const communityData = communitySnap.data();
-      await membersRef.doc(userId).set({
-        "createdAt": new Date(),
-        "userId": userId,
-      });
-      await communityRef.update({
-        "memberCount": communityData.memberCount + 1,
-      });
-      res.status(200).send(true);
-    },
-);
+exports.followed = onDocumentCreated("users/{userId}/followers/{followingId}", async (event) => {
+  const userId = event.params.userId;
+  const userRef = db.collection("users").doc(userId);
+  const userSnap = await userRef.get();
+  const userData = userSnap.data();
+  await userRef.update({followersCount: userData.followersCount + 1});
+});
 
+exports.unfollowing = onDocumentDeleted("users/{userId}/following/{followingId}", async (event) => {
+  const userId = event.params.userId;
+  const userRef = db.collection("users").doc(userId);
+  const userSnap = await userRef.get();
+  const userData = userSnap.data();
+  await userRef.update({followingCount: userData.followingCount - 1});
+});
+
+exports.unfollowed = onDocumentDeleted("users/{userId}/followers/{followingId}", async (event) => {
+  const userId = event.params.userId;
+  const userRef = db.collection("users").doc(userId);
+  const userSnap = await userRef.get();
+  const userData = userSnap.data();
+  await userRef.update({followersCount: userData.followersCount - 1});
+});
+
+exports.joinCommunty = onDocumentCreated("communities/{communityId}/members/{memberId}", async (event) => {
+  const communityId = event.params.communityId;
+  const communityRef = db.collection("communities").doc(communityId);
+  const communitySnap = await communityRef.get();
+  const communityData = communitySnap.data();
+  await communityRef.update({"memberCount": communityData.memberCount + 1});
+});
+
+exports.leaveCommunty = onDocumentDeleted("communities/{communityId}/members/{memberId}", async (event) => {
+  const communityId = event.params.communityId;
+  const communityRef = db.collection("communities").doc(communityId);
+  const communitySnap = await communityRef.get();
+  const communityData = communitySnap.data();
+  await communityRef.update({"memberCount": communityData.memberCount - 1});
+});
